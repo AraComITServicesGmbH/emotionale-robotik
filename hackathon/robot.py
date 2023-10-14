41af37cb77199651622cb6e8a46dde37bf943203
DJE98 <dennis@eisermann.one>
2023-10-14 23:03:41 +0200

    Added docstring for update

--- hackathon/robot.py
+++ hackathon/robot.py
@@ -2,4 +2,11 @@ class Robot:
     update_interval = 0.5 
     
     def update(self,  image, emotions):
+        """
+        Updates the current instance with a new image and associated emotions.
+
+        Parameters:
+        - image (numpy.ndarray): The BGR image to be processed.
+        - emotions (list): A list containing the emotions associated with the image.
+        """
         raise NotImplementedError()
