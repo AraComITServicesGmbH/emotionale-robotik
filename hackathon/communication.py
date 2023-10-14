162e2ea95353d1a9f798d05c801b0a23e72fb878
DJE98 <dennis@eisermann.one>
2023-10-14 13:53:31 +0200

    Docstring for create_server_socket

--- hackathon/communication.py
+++ hackathon/communication.py
@@ -21,6 +21,19 @@ def create_client_socket():
 
 
 def create_server_socket(ip, port):
+    """
+    Creates a server socket bound to the specified IP address and port.
+
+    This function initializes a new server socket using the IPv4 address family
+    and the TCP protocol. It then binds the socket to the given IP address and port.
+
+    Parameters:
+    - ip (str): The IP address to which the server socket should be bound.
+    - port (int): The port number to which the server socket should be bound.
+
+    Returns:
+        socket: A bound TCP server socket using IPv4.
+    """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((ip, port))
     return server_socket
